85666da1ddfb557543d00f41efdd79573f805020
Kevin <kfornier@gmail.com>
2026-06-27 23:46:11 -0300

    first

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module org.kevin.imagemirador {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.dsf.imagemirador to javafx.fxml;
+    exports org.dsf.imagemirador;
+    exports org.dsf.imagemirador.Controller;
+    opens org.dsf.imagemirador.Controller to javafx.fxml;
+}
