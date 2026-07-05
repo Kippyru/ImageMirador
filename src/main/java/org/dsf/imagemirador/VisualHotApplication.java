84ee36be51ac9b913329cf056bbfb4d87d78805f
Kevin <kfornier@gmail.com>
2026-07-05 19:24:35 -0300

    chore: Change name of the app

--- src/main/java/org/dsf/imagemirador/VisualHotApplication.java
+++ src/main/java/org/dsf/imagemirador/VisualHotApplication.java
@@ -12,7 +12,7 @@ public class VisualHotApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(VisualHotApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("VisualHot!");
         stage.setScene(scene);
         stage.show();
     }
