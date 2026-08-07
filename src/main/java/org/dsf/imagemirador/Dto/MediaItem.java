0dde0d0221113ccc36b3a8d85aa7067b92b82fcc
Kevin <kfornier@gmail.com>
2026-08-07 20:41:03 -0300

    refactor(dto): add enum MediaType

--- src/main/java/org/dsf/imagemirador/Dto/MediaItem.java
+++ src/main/java/org/dsf/imagemirador/Dto/MediaItem.java
@@ -1,6 +1,6 @@
 package org.dsf.imagemirador.Dto;
 
-public record MediaItem(String path, String name, String type) {
-
-
+//agregue un enum, asi el dto soporta otras media y no estamos pasando un string como uga uga
+public record MediaItem(String path, String name, MediaType type) {
+    public enum MediaType { IMAGE, VIDEO, AUDIO } //el enum deberia ser un archivo aparte peroo.... anidar anidar
 }
