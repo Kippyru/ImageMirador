65a5f360dcebe36731601d78c1adf6436b61d552
Kevin <kfornier@gmail.com>
2026-08-08 18:24:25 -0300

    feat(gallery): add gallery view and gallery controller

---
+++ src/main/java/org/dsf/imagemirador/Controller/GalleryController.java
@@ -0,0 +1,13 @@
+package org.dsf.imagemirador.Controller;    //si o si se necesita otro controller ya que no se puede atar 2 fxml a uno
+
+import javafx.fxml.FXML;
+
+public class GalleryController {
+
+    //en este controller se puede poner las miniaturas creooo o eso se haria en el toggleGalleryMethod que esta en el MainController?
+    //y abria que hacer un viewer nuevo que maneje esa logica si o si
+    @FXML
+    public void initialize() {
+        System.out.println("Controlador de galeria anda uwu");
+    }
+}
